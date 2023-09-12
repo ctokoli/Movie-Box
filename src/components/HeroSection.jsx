ed6f9614166821d96d9b50cdf6d411743e7c5944
ctokoli <matumasie@gmail.com>
2023-09-12 14:02:17 +0000

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -38,7 +38,7 @@ const MenuWithHero = () => {
                             <div className="description-text">
                                 <p>
                                    John Wick is on the run after killing a member of the international 
-                                   assassins' guild, and with a $14 million price tag on his head, 
+                                   assassins&apos; guild, and with a $14 million price tag on his head, 
                                    he is the target of hit men and women everywhere. 
                                 </p>
                                 <button className="playButton">
