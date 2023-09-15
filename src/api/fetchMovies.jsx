4eb6870297336d0b7040c6b81205af817b3f82f7
ctokoli <matumasie@gmail.com>
2023-09-15 22:38:43 +0000

    Update fetchMovies.jsx

--- src/api/fetchMovies.jsx
+++ src/api/fetchMovies.jsx
@@ -7,7 +7,26 @@ const FetchMovies = async () => {
     }
     };
     const data = await fetch(url, options);
-    const result = await data.json()
+    const sliceData = await data.json()
+    const result = sliceData?.results?.slice(0, 10);
+    return result;
+}
+
+export const FetchMovie = async (id) => {
+    const appendurl = "?&append_to_response=videos"
+    const url = import.meta.env.VITE_APP_API_KEY_MOVIE + id + appendurl;
+    console.log(url)
+    const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: import.meta.env.VITE_APP_API_TOKEN
+    }
+    };
+    const data = await fetch(url, options);
+    const sliceData = await data.json()
+    const result = sliceData;
+    console.log(result)
     return result;
 }
  
