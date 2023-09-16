1af52b1eae1397b03234fc4f4e3ac5fe3b9593a1
ctokoli <matumasie@gmail.com>
2023-09-16 18:37:21 +0000

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,13 @@
 import MenuWithHero from "./HeroSection";
 import FetchMovies from "../api/fetchMovies";
+import FooterSection from "./FooterSection";
 
 const HomePage = () => {
     FetchMovies();
     return ( 
         <>
           <MenuWithHero />
+          <FooterSection />
         </>
      );
 }
