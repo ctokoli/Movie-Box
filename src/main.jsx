4a409438db86fff50e95dde5044e9bd37db6e866
ctokoli <matumasie@gmail.com>
2023-09-16 21:41:00 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React from 'react'
 import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import ReactDOM from 'react-dom/client'
@@ -9,7 +8,8 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
       <App />
-    
+    </QueryClientProvider>
   </React.StrictMode>,
 )
