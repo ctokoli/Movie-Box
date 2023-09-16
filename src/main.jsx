8f12e2fb00da0ebaf6729ed17aa0db63113cb7a3
ctokoli <matumasie@gmail.com>
2023-09-16 21:17:42 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
+import App from './Index'
 import './index.css'
 
 const queryClient = new QueryClient()
