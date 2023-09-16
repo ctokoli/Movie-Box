2767bf3833980cb4f78c3c6ded2c37a4f99dc210
ctokoli <matumasie@gmail.com>
2023-09-16 18:37:21 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -11,7 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>,
 )
